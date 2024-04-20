da1d67cc51c9d6a8d49bd0ea19fcc3fee9b9c19c
Kristopher Wilson <kristopherwilson@gmail.com>
2024-04-20 10:42:07 -0400

    Adds boxscore partial

--- app/utils/liveGame.ts
+++ app/utils/liveGame.ts
@@ -1,6 +1,6 @@
 // TODO: migrate from ../utils
 
-import { LiveGame, Team } from '../types';
+import { LiveGame, LiveGame_LiveData_LineScore, Team } from '../types';
 
 export const followedHomeOrAway = (game: LiveGame, followedTeam: Team) =>
   game.gameData.teams.away.id === followedTeam.id ? 'away' : 'home';
@@ -18,5 +18,35 @@ export const opponentTeam = (game: LiveGame, followedTeam: Team) =>
 export const playerBoxscore = (
   game: LiveGame,
   homeOrAway: 'home' | 'away',
-  playerId: number
+  playerId: number,
 ) => game.liveData.boxscore.teams[homeOrAway].players[`ID${playerId}`];
+
+export const boxscore = (game: LiveGame) => {
+  const rawInnings = game.liveData.linescore.innings;
+  const scheduledInnings = game.liveData.linescore.scheduledInnings;
+
+  let innings = rawInnings;
+
+  if (innings.length > scheduledInnings) {
+    innings = rawInnings.slice(-scheduledInnings);
+  } else if (innings.length < scheduledInnings) {
+    const remainingInnings = scheduledInnings - innings.length;
+
+    innings = rawInnings.concat(
+      Array.from(Array(remainingInnings).keys()).map(
+        (k) =>
+          ({
+            num: k + 1 + rawInnings.length,
+          }) as LiveGame_LiveData_LineScore['innings'][0],
+      ),
+    );
+  }
+
+  return {
+    innings,
+    totalInnings: game.liveData.linescore.scheduledInnings,
+    totals: game.liveData.linescore.teams,
+    renderInningsStart:
+      Math.max(game.liveData.linescore.currentInning - 9, 0) + 1,
+  };
+};
